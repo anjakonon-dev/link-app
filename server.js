8ae10acc06d05bc6ef8f5699a774ea84b65f44be
anjakonon-dev <anjakonon@gmail.com>
2026-01-14 06:15:26 +0300

    server.js

--- server.js
+++ server.js
@@ -31,5 +31,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+http.listen(PORT, HOST, () => console.log(`Сервер запущен на http://${HOST}:${PORT}`));
 http.listen(PORT, () => console.log(`Сервер запущен на ${PORT}`));
